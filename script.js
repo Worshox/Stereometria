e730937cb6770fd7a72fe718d881f662f6dc1238
Worshox <worshox.28@gmail.com>
2023-01-03 00:54:22 +0100

    Improve responsivity and quality of shapes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+
 function main() {
     const canvas = document.querySelector('#c');
     const renderer = new THREE.WebGLRenderer({canvas});
@@ -41,8 +43,29 @@ function main() {
         makeInstance(geometry, 0x00ccff, 2),
     ];
 
+    function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+            renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
+
     function render(time) {
         time *= 0.001;
+        
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
+
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
     
         cubes.forEach((cube, ndx) => {
           const speed = 1 + ndx * .1;
