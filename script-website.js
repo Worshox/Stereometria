eb4af420e22dfc90611b4787533f23e568c2b73b
Worshox <worshox.28@gmail.com>
2023-01-08 23:56:13 +0100

    Remove animation error

--- script-website.js
+++ script-website.js
@@ -3,8 +3,6 @@ Array.from(document.getElementsByTagName('button')).forEach( element => {
         console.log(element);
         if (element.nextElementSibling.classList.value){
             element.nextElementSibling.classList = "";
-            let styles = window.getComputedStyle(element.querySelector('h2'), ':after');
-            styles.setProperty('animation', 'arrowRotate');
         } else {
             element.nextElementSibling.classList = "rolled";
         }  
