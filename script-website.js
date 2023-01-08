2ae56e986a58a435acb5548d58d037df66dc63d8
Worshox <worshox.28@gmail.com>
2023-01-08 23:54:42 +0100

    Try to animate navbar arrows

--- script-website.js
+++ script-website.js
@@ -1,8 +1,12 @@
 Array.from(document.getElementsByTagName('button')).forEach( element => {
     element.addEventListener('click', event => {
-        if (element.nextElementSibling.classList.value)
+        console.log(element);
+        if (element.nextElementSibling.classList.value){
             element.nextElementSibling.classList = "";
-        else
-            element.nextElementSibling.classList = "rolled";    
+            let styles = window.getComputedStyle(element.querySelector('h2'), ':after');
+            styles.setProperty('animation', 'arrowRotate');
+        } else {
+            element.nextElementSibling.classList = "rolled";
+        }  
     });
 });
